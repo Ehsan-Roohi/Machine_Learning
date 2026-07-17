b9f5fac5e4d3c137a9d8b4bbcf45e9e161354eeb
Ehsan-Roohi <roohie@umass.edu>
2026-07-17 19:03:59 -0400

    Correct diffuse-wall note in R13 cavity output

--- ci_r13/run_case.sh
+++ ci_r13/run_case.sh
@@ -12,6 +12,7 @@ geoToH5 lid.geo lid5.h5 "-setnumber p 5"
 geoToH5 lid.geo lid6.h5 "-setnumber p 6"
 cp /work/input_jfm_kn005_u100.yml .
 PYTHONPATH=/work/src python3 /work/run_and_postprocess.py 2>&1 | tee /work/output/run.log
+sed -i 's/Walls use the repository-standard chi_tilde=1.0 and theta_w=1.0./Fully diffuse Maxwell walls use chi=1, mapped to chi_tilde=sqrt(2\/pi)=0.797884560803; theta_w=1.0./' /work/output/RUN_NOTES.txt
 cp /work/input_jfm_kn005_u100.yml /work/output/
 test -s /work/output/run.log
 test -s /work/output/summary_all_meshes.json
